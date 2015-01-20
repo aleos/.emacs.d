6e9d14bdaeefdafbf1e06c2e775761896c0fadb1
aleos <aleos@localhost>
2015-01-20 13:05:02 +0300

    + init-load-packages.el for loading additional packages from the plugins folder

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 ;; Very good source of recipes about emacs configuring:
 ;; http://www.masteringemacs.org/article/beginners-guide-to-emacs
 
+(progn (message "+ init.el"))
+
 ;; Enable IDO (“Interactively Do Things”) mode
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
@@ -16,29 +18,13 @@
 ;; Don't show toolbar
 (setq tool-bar-mode nil)
 
-(add-to-list 'load-path "~/.emacs.d/plugins")
-;;(require 'qt-pro)
-;;(add-to-list 'auto-mode-alist '("\\pr[io]$" . qt-pro-mode))
-(require 'qmake-mode "qmake.el")
-(add-to-list 'auto-mode-alist '("\\pr[io]$" . qmake-mode))
-
-;;;; This snippet enables lua-mode
-;; This line is not necessary, if lua-mode.el is already on your load-path
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
-
-;;--- Load mmix-mode and use it for .mms files -----------
-(autoload 'mmix-mode "mmix-mode" "Major mode for editing MMIX files" t)
-(setq auto-mode-alist (cons '("\\.mms" . mmix-mode)
-                                  auto-mode-alist)) 
-
+(load "~/.emacs.d/init-load-packages.el")
 ;; check OS type
 (cond
  ((string-equal system-type "windows-nt") ; Microsoft Windows
   (progn
     (message "Microsoft Windows") )
-  (load-library "~/.emacs.d/init-win.el")
+  (load "~/.emacs.d/init-win.el")
   )
  ((string-equal system-type "darwin")   ; Mac OS X
   (progn
